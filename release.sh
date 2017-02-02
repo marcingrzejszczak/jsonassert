4d442333c6448c4706182b5f3a23917756779fc6
Marcin Grzejszczak <marcin@grzejszczak.pl>
2017-02-02 11:54:11 +0100

    Updated instruction for releases

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-git fetch && ./gradlew clean build && ./gradlew createRelease && ./gradlew uploadArchives && ./gradlew closeRepository && ./gradlew promoteRepository && ./gradlew pushRelease
+git fetch --tags && ./gradlew clean build && ./gradlew createRelease && ./gradlew uploadArchives && ./gradlew closeRepository && ./gradlew promoteRepository && ./gradlew pushRelease
