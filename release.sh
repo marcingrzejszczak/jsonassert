54e51a006937d45b2d64666cf72748df3d90aa26
Marcin Grzejszczak <marcin@grzejszczak.pl>
2018-04-12 12:20:31 +0200

    test

--- release.sh
+++ release.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+echo "foo"
 git fetch --tags && ./gradlew clean build && ./gradlew createRelease && ./gradlew uploadArchives && ./gradlew closeAndReleaseRepository && git push origin master --tags
