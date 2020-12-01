05e3d380a5160069639ec3c399e60b7a31479fe7
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-12-01 10:37:12 +0100

    Setting up release scripts

--- release.sh
+++ release.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-git fetch --tags && ./gradlew clean build && ./gradlew createRelease && ./gradlew uploadArchives && ./gradlew closeAndReleaseRepository && git push origin master --tags
+
+set -o errexit
+
+export DEV_VERSION="${DEV_VERSION:?You must set the next dev version}"
+export RELEASE_VERSION="${RELEASE_VERSION:?You must set the next release version}"
+
+echo "Will release [${RELEASE_VERSION}] and then bump to dev version [${DEV_VERSION}]"
+git fetch --tags && \
+  ./mvnw -B -Dtag=v"${RELEASE_VERSION}" release:clean release:prepare -DreleaseVersion="${RELEASE_VERSION}" -DdevelopmentVersion="${DEV_VERSION}" && \
+  ./mvnw -B -Dtag=v"${RELEASE_VERSION}" -DreleaseVersion="${RELEASE_VERSION}" -DdevelopmentVersion="${DEV_VERSION}" -Dgoals=deploy release:perform && \
+  git push origin master --tags
