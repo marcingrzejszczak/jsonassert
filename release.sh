f2826e30809938757ee37db40d2a439ee0c9e767
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-04-13 14:51:56 +0200

    Added missing central profile

--- release.sh
+++ release.sh
@@ -8,5 +8,5 @@ export RELEASE_VERSION="${RELEASE_VERSION:?You must set the next release version
 echo "Will release [${RELEASE_VERSION}] and then bump to dev version [${DEV_VERSION}]"
 git fetch --tags && \
   ./mvnw -B -Dtag=v"${RELEASE_VERSION}" release:clean release:prepare -DreleaseVersion="${RELEASE_VERSION}" -DdevelopmentVersion="${DEV_VERSION}" && \
-  ./mvnw -B -Dtag=v"${RELEASE_VERSION}" -DreleaseVersion="${RELEASE_VERSION}" -DdevelopmentVersion="${DEV_VERSION}" -Dgoals=deploy release:perform && \
+  ./mvnw -B -Dtag=v"${RELEASE_VERSION}" -DreleaseVersion="${RELEASE_VERSION}" -DdevelopmentVersion="${DEV_VERSION}" -Dgoals=deploy release:perform -Pcentral && \
   git push origin master --tags
